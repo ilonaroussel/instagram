fa5810ec197fe280381129409d41da9eb5b03c37
Florian-Esc <fescolivet@gmail.com>
2026-02-10 14:57:38 +0100

    début Story Post

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@
 import express from "express";
 import cors from "cors";
 import { posts } from "./data/posts";
+import { addPost } from "./data/addPost"
+
 
 const app = express();
 const PORT = 3001;
@@ -17,3 +19,12 @@ app.get("/posts", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+app.post("/posts", (req, res) => {
+  try {
+    const post = addPost(req.body.content, req.body.userId)
+    res.status(201).json(post)
+  } catch (error) {
+    res.status(400).json({ message: (error as Error).message })
+  }
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,30 @@
 // App.tsx
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Feed from "./pages/Feed";
+import PostForm from "./PostForm";
+//import Register from "./pages/Register";
+//import Login from "./pages/Login";
+//import Feed from "./pages/Feed";
 
 function App(){
+  const handlePostCreated = (post: any) => {
+    console.log("Post created:", post);
+  };
+
   return (
     <BrowserRouter>
+      <Routes>
+        <Route path="/PostForm" element={<PostForm onPostCreated={handlePostCreated} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+    /*<BrowserRouter>
       <Routes>
         <Route path="/" element={<Feed />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
-  );
-};
-
+    </BrowserRouter>*/
 
 export default App;
